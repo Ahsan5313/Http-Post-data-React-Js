d4b576de7b672658c0f2cb6b2ee093ee3d164c5c
Freelancer Ahsan <81156921+Ahsan5313@users.noreply.github.com>
2021-07-21 13:52:16 +0600

    add React Js

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Hello  from './Components/Hello'
+import Welcome from './Components/Welcome'
+
+
+export default function App() {
+  return (
+    <div>
+       
+
+   
+     
+       
+    </div>
+  )
+}
